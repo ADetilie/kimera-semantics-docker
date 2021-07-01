768494fa2f3652d3ab0b55fd7cffbf637293378e
Oleksandr Detilie <al.detillier@gmail.com>
2021-07-01 14:48:17 +0300

    feat: add script to build all dockerfiles at once

---
+++ build_all_layers.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+echo "================================================"
+echo "START BUILDING DOCKER LAYERS"
+echo "================================================"
+
+echo "=== layer_base ===" && \
+cd layer_base && ./build_image.sh && \
+echo "=== layer_kimera_semantics_installed ===" && \
+cd ../layer_kimera_semantics_installed && ./build_image.sh && \
+echo "=== layer_start_kimera_entrypoint ===" && \
+cd ../layer_start_kimera_entrypoint && ./build_image.sh && \
+echo "=== SUCCESS ===" || \
+echo "=== FAILURE ==="
+
+echo "================================================"
+echo "FINISH BUILDING DOCKER LAYERS"
+echo "================================================"
